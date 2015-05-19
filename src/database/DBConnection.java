e2a43d8f493f9e6880d81b81a9e563b10c3c3c48
dennis.vaerum@gmail.com <dvarum12@student.aau.dk>
2015-05-19 13:40:27 +0200

    mit arbejde

--- src/database/DBConnection.java
+++ src/database/DBConnection.java
@@ -1,5 +1,152 @@
 package database;
 
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DBConnection {
+
+    // Database credentials
+    private String DATABASE_NAME = "dmab0914_2Sem_6";
+
+    // JDBC driver name and database URL
+    private static String JDBC_DRIVER = "java.sql.Driver";
+    private String DB_URL = "jdbc:sqlserver://kraka.ucn.dk;databaseName="
+            + DATABASE_NAME;
+
+    private static DBConnection instance = null;
+    private static Connection conn = null;
+
+    private DBConnection() throws ClassNotFoundException, SQLException {
+//		try {
+        // Register JDBC driver
+        Class.forName(JDBC_DRIVER);
+//		} catch (ClassNotFoundException e) {
+//			e.printStackTrace();
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+    }
+
+    public static DBConnection getInstance() throws ClassNotFoundException, SQLException {
+        if (instance == null) {
+            instance = new DBConnection();
+        }
+        return instance;
+    }
+
+    public void connect() throws SQLException, IOException, NoSuchFileException {
+        // Open a connection
+        Path file_user = Paths.get("username.txt");
+        Path file_pass = Paths.get("password.txt");
+
+        String user = new String(Files.readAllBytes(file_user));
+        String pass = new String(Files.readAllBytes(file_pass));
+
+        conn = DriverManager.getConnection(DB_URL, user, pass);
+    }
+
+    public Connection getConn() throws IOException {
+        try {
+            if (conn.isClosed()) {
+                connect();
+            }
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return conn;
+    }
+
+    public void disconnect() {
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public int sendInsertSQL(String sql) {
+        ResultSet rs;
+        Statement stmt;
+        int r = 0;
+
+        try {
+            stmt = conn.createStatement();
+            r = stmt.executeUpdate(sql);
+            rs = stmt.executeQuery("SELECT SCOPE_IDENTITY();");
+            rs.next();
+            r = rs.getInt(1);
+            rs.close();
+            stmt.close();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        // Returns the ID for the new Row
+        return r;
+    }
+
+    public int sendUpdateSQL(String sql) {
+        Statement stmt;
+        int r = 0;
+
+        try {
+            stmt = conn.createStatement();
+            r = stmt.executeUpdate(sql);
+            stmt.close();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        // Returns a number equal to the amount of rows that has been change
+        return r;
+    }
+
+    public Object sendSQL(Object obj, String sql, String method_name) {
+        ResultSet rs = null;
+        Method method = null;
+        Object r = null;
+        Statement stmt;
+
+        try {
+
+            try {
+                method = obj.getClass().getMethod(method_name, ResultSet.class);
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            }
+
+            stmt = conn.createStatement();
+            try {
+                rs = stmt.executeQuery(sql);
+                r = method.invoke(obj, rs);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+            stmt.close();
+
+            return r;
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return rs;
+    }
 
 }
---
+++ src/test/database/DBConnectionTest.java
@@ -0,0 +1,128 @@
+package database;
+
+import org.junit.BeforeClass;
+import org.junit.AfterClass;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.nio.file.NoSuchFileException;
+import java.sql.SQLException;
+
+import static org.junit.Assert.*;
+
+/**
+ * DBConnection Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>May 19, 2015</pre>
+ */
+public class DBConnectionTest {
+    private static DBConnection instance = null;
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        try {
+            instance = DBConnection.getInstance();
+            instance.connect();
+        } catch (ClassNotFoundException | SQLException | NoSuchFileException e) {
+        }
+    }
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        try {
+            instance.sendUpdateSQL("delete from Customer;");
+        } catch (NullPointerException e) {
+        }
+        instance.disconnect();
+    }
+
+    /**
+     * Method: getInstance()
+     */
+    @Test
+    public void testGetInstance() throws Exception {
+        try {
+            DBConnection instance = DBConnection.getInstance();
+            assertNotNull(instance);
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+//			e.printStackTrace();
+            fail("Lib: '" + e.getMessage() + "' is missing");
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+//			e.printStackTrace();
+            fail("kunne ikke forbinde til databasen" + e.getMessage());
+        }
+    }
+
+    /**
+     * Method: connect()
+     */
+    @Test
+    public void testConnect() throws Exception {
+        try {
+            DBConnection.getInstance().connect();
+        } catch (SQLException e) {
+            fail(e.getMessage());
+        } catch (NoSuchFileException e) {
+            fail("The file '" + e.getFile() + "' does not exist at '" + System.getProperty("user.dir") + "'");
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    /**
+     * Method: getConn()
+     */
+    @Test
+    public void testGetConn() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    /**
+     * Method: disconnect()
+     */
+    @Test
+    public void testDisconnect() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    /**
+     * Method: sendInsertSQL(String sql)
+     */
+    @Test
+    public void testSendInsertSQL() throws Exception {
+        int r = instance.sendInsertSQL("INSERT into Customer values('Muahaha','Like','1337','awesome','42884242','private');");
+        assertTrue(r > 0);
+    }
+
+    /**
+     * Method: sendUpdateSQL(String sql)
+     */
+    @Test
+    public void testSendUpdateSQL() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    /**
+     * Method: sendSQL(Object obj, String sql, String method_name)
+     */
+    @Test
+    public void testSendSQL() throws Exception {
+        //TODO: Test goes here...
+    }
+} 
