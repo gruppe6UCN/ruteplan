e8319ede5669efa98651b5cf4b007e25b1e95cf7
dennis.vaerum@gmail.com <dvarum12@student.aau.dk>
2015-05-21 09:49:26 +0200

    updated: to match database

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -6,59 +6,21 @@ package model;
  */
 
 public class Customer {
-	
 	private long id;
-	private String name;
-	private String adress;
-	
-	public Customer(long id, String name, String adress) {
-		super();
+	private String streetName;
+	private String streetNo;
+	private int zipcode;
+	private String city;
+
+	public Customer(long id, String streetName, String streetNo, int zipcode, String city) {
 		this.id = id;
-		this.name = name;
-		this.adress = adress;
+		this.streetName = streetName;
+		this.streetNo = streetNo;
+		this.zipcode = zipcode;
+		this.city = city;
 	}
-	
-	
-	/**
-	 * @return the id
-	 */
-	public long getId() {
+
+	public long getID() {
 		return id;
 	}
-
-	/**
-	 * @param id the id to set
-	 */
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
-
-	/**
-	 * @param name the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	/**
-	 * @return the adress
-	 */
-	public String getAdress() {
-		return adress;
-	}
-
-	/**
-	 * @param adress the adress to set
-	 */
-	public void setAdress(String adress) {
-		this.adress = adress;
-	}
-
 }
