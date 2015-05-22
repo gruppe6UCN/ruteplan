6895f78afb19291cd8f74fbda36ff7400271835d
dennis.vaerum@gmail.com <dvarum12@student.aau.dk>
2015-05-22 12:01:09 +0200

    trying enums

--- src/try_stuff/try_enum.java
+++ src/try_stuff/try_enum.java
@@ -1,10 +1,13 @@
 package try_stuff;
 
+import model.Type;
+
 /**
  * Created by alt_mulig on 5/22/15.
  */
 public class try_enum {
     public static void main(String[] args) {
-
+        System.out.println(Type.Standard.toString());
+        System.out.println(Type.Standard.name());
     }
 }
