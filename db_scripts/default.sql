643403d579b8d88b986f6807a2d5f5b8a8db97d4
dennis.vaerum@gmail.com <dvarum12@student.aau.dk>
2015-05-24 11:44:41 +0200

    database dummy

--- db_scripts/default.sql
+++ db_scripts/default.sql
@@ -1,1 +1,9 @@
- 
+delete from DefaultRoute
+
+INSERT into DefaultRoute values('Stor', '1:00:00', 0);
+INSERT into DefaultRoute values('Stor', '2:00:00', 0);
+INSERT into DefaultRoute values('Stor', '3:00:00', 0);
+INSERT into DefaultRoute values('Stor', '4:00:00', 0);
+INSERT into DefaultRoute values('Stor', '5:00:00', 0);
+
+select * from DefaultRoute
