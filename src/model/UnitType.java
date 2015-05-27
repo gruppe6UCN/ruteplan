47b8010924989840a47da12260c15b698a16daf8
dennis.vaerum@gmail.com <dvarum12@student.aau.dk>
2015-05-27 13:21:30 +0200

    added: transport units to UnitType enum

--- src/model/UnitType.java
+++ src/model/UnitType.java
@@ -1,8 +1,15 @@
 package model;
 
 public enum UnitType {
-	Standard (1);
-	
+	EU_PAL          (1),
+	HALV_PAL	    (0.5),
+	KVART_PAL	    (0.25),
+	RULLE_PALLE	    (0.70),
+    VARE_CONTAINER  (0.44),
+    SMØR_CONTAINER  (0.40),
+    MÆLKE_CONTAINER (0.40);
+
+
 	private final double size;
 	
 	UnitType(double size) {
