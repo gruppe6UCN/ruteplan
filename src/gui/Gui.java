7a6bd4dc6bdb68afddc6ecb90db13e315fba76ac
dennis.vaerum@gmail.com <dvarum12@student.aau.dk>
2015-05-28 03:01:28 +0200

    Gui virker

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -1,16 +1,17 @@
 package gui;
 
+import control.ImportController;
+
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.Vector;
 
 
 public class Gui extends JFrame {
 
     private JPanel contentPane;
     private JTable table;
+    private ImportController importController = ImportController.getInstance();
 
     /**
      * Launch the application.
@@ -20,7 +21,7 @@ public class Gui extends JFrame {
             public void run() {
                 try {
                     Gui frame = new Gui();
-                    frame.setVisible(true);
+//                    frame.setVisible(true);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -32,52 +33,30 @@ public class Gui extends JFrame {
      * Create the frame.
      */
     public Gui() {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 450, 300);
-        contentPane = new JPanel();
-        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        setContentPane(contentPane);
-        contentPane.setLayout(null);
+        Vector<String> columnNames = new Vector<String>();
 
-        // Import the default route
-        JButton load = new JButton("Load");
-        load.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
-                control.ImportController.getInstance().importRoutes();
-            }
-        });
-        load.setBounds(10, 228, 89, 23);
-        contentPane.add(load);
+        columnNames.addElement("Rute ID");
+        columnNames.addElement("Default Rute ID");
+        columnNames.addElement("Antal Stop");
 
-        //Optimise the route
-        JButton optimere = new JButton("Optimere");
-        optimere.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
-                control.OptimizeController.getInstance().optimize();
-            }
-        });
-        optimere.setBounds(168, 228, 89, 23);
-        contentPane.add(optimere);
+        Vector<Vector> rowData = new Vector<Vector>();
 
-        //Export the new route
-        JButton gem = new JButton("Gem");
-        gem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
-                control.ExportController.getInstance().exportDatas();
-            }
-        });
-        gem.setBounds(335, 228, 89, 23);
-        contentPane.add(gem);
+        JTable table = new JTable(rowData, columnNames);
+        JScrollPane scrollPane = new JScrollPane(table);
 
-        ScrollPane scrollPane = new ScrollPane();
-        scrollPane.setBackground(Color.WHITE);
-        scrollPane.setBounds(10, 10, 414, 210);
-        contentPane.add(scrollPane);
 
-        table = new JTable();
-        table.setForeground(Color.WHITE);
-        table.setBounds(10, 11, 414, 209);
-        contentPane.add(table);
+        JFrame frame = new JFrame("Table Printing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.add(scrollPane, BorderLayout.CENTER);
+        scrollPane.setCorner(JScrollPane.UPPER_RIGHT_CORNER, new JButton("..."));
+
+
+        frame.setSize(300, 150);
+        frame.setVisible(true);
+
+
+        // Prof
+        importController.importRoutes(rowData);
     }
 }
-
