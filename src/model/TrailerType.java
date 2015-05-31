0040161c5d23d82d90fc2d11813883d686275798
dennis.vaerum@gmail.com <dvarum12@student.aau.dk>
2015-05-31 21:09:59 +0200

    corrected the measurement for the 'STOR' Trailer

--- src/model/TrailerType.java
+++ src/model/TrailerType.java
@@ -1,7 +1,8 @@
 package model;
 
 public enum TrailerType {
-    STOR (33);
+    // Measured in 'rulle-paller', if I remember correctly a EU-pal is 1.4 'rulle-palle'
+    STOR (51.0);
     
     private final double capacity;
     
