97662ab384ffe428a35efe664be82fb8742dae05
dennis.vaerum@gmail.com <dvarum12@student.aau.dk>
2015-06-01 10:54:01 +0200

    Added parallelStream, but I do not know if it makes it faster

--- src/try_stuff/findAndMergeCycles.java
+++ src/try_stuff/findAndMergeCycles.java
@@ -12,99 +12,107 @@ import org.jgrapht.graph.ListenableDirectedWeightedGraph;
 import java.awt.geom.Point2D;
 import java.io.*;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Set;
+import java.util.*;
 
 public class findAndMergeCycles {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException, IOException {
-        DBConnection dbConnection = DBConnection.getInstance();
-        DBRoad dbRoad = DBRoad.getInstance();
-        DBGeoLoc dbGeoLoc = DBGeoLoc.getInstance();
+        while (true) {
+            DBConnection dbConnection = DBConnection.getInstance();
+            DBRoad dbRoad = DBRoad.getInstance();
+            DBGeoLoc dbGeoLoc = DBGeoLoc.getInstance();
 
-        ListenableDirectedWeightedGraph<GeoLoc, Edge> map
-                = new ListenableDirectedWeightedGraph<>(Edge.class);
+            ListenableDirectedWeightedGraph<GeoLoc, Edge> map
+                    = new ListenableDirectedWeightedGraph<>(Edge.class);
 
-        ScriptRunner runner = new ScriptRunner(dbConnection.getConn(), false, true);
-        runner.runScript(new BufferedReader(new FileReader("db_scripts/create_arlas_db.sql")));
+            ScriptRunner runner = new ScriptRunner(dbConnection.getConn(), false, true);
+            runner.runScript(new BufferedReader(new FileReader("db_scripts/create_arlas_db.sql")));
 
 
-        HashMap<Long, GeoLoc> geoLocs = new HashMap<>();
-        ArrayList<Edge> edges = new ArrayList<>();
-        ArrayList<Road> roads = dbRoad.getRoads();
+            Map<Long, GeoLoc> geoLocs = Collections.synchronizedMap(new HashMap<>());
+            List<Edge> edges = Collections.synchronizedList(new ArrayList<>());
+            ArrayList<Road> roads = dbRoad.getRoads();
 
-        roads.forEach(road -> {
-            GeoLoc geoLocFrom = dbGeoLoc.getGeoLoc(road.getFrom());
-            GeoLoc geoLocTo = dbGeoLoc.getGeoLoc(road.getTo());
-            if (!geoLocs.containsKey(geoLocFrom.getID())) {
-                geoLocs.put(geoLocFrom.getID(), geoLocFrom);
-                map.addVertex(geoLocFrom);
-            }
-            if (!geoLocs.containsKey(geoLocTo.getID())) {
-                geoLocs.put(geoLocTo.getID(), geoLocTo);
-                map.addVertex(geoLocTo);
-            }
-
-            Edge edge = map.addEdge(
-                    geoLocs.get(geoLocFrom.getID()),
-                    geoLocs.get(geoLocTo.getID()));
-            map.setEdgeWeight(edge, road.getDistance());
-
-            edges.add(edge);
-        });
-
-        ArrayList<Set<GeoLoc>> cycleOfVertex = new ArrayList<Set<GeoLoc>>();
-        geoLocs.forEach((aLong, geoLoc) -> {
-            boolean geoLocIsInCycle = cycleOfVertex.stream().anyMatch(geoLocs1 -> geoLocs1.contains(geoLoc));
-            ArrayList<Set<GeoLoc>> t = cycleOfVertex;
-            if (!geoLocIsInCycle) {
-                cycleOfVertex.add(new CycleDetector<GeoLoc, Edge>(map).findCyclesContainingVertex(geoLoc));
-            }
-        });
-
-        System.out.println(String.format("There is %d cycle(s)", cycleOfVertex.size()));
-
-        if (cycleOfVertex.size() > 1) {
-            HashMap<Double, Road> _roads = new HashMap<>();
-
-            cycleOfVertex.stream().forEach(cycleA -> {
-                cycleOfVertex.stream().forEach((cycleB) -> {
-                    if (cycleA != cycleB) {
-                        cycleA.stream().forEach(geoLocA -> {
-                            cycleB.stream().forEach(geoLocB -> {
-                                Double d = distFrom(geoLocA.getLocation(), geoLocB.getLocation());
-                                if (d >= 0.1) {
-                                    _roads.put(d, new Road(geoLocA.getID(), geoLocB.getID(), d, null));
-                                }
-                            });
-                        });
+            roads.parallelStream().forEach(road -> {
+                GeoLoc geoLocFrom = dbGeoLoc.getGeoLoc(road.getFrom());
+                GeoLoc geoLocTo = dbGeoLoc.getGeoLoc(road.getTo());
+                if (!geoLocs.containsKey(geoLocFrom.getID())) {
+                    geoLocs.put(geoLocFrom.getID(), geoLocFrom);
+                    synchronized (map) {
+                        map.addVertex(geoLocFrom);
                     }
-                });
+                }
+                if (!geoLocs.containsKey(geoLocTo.getID())) {
+                    geoLocs.put(geoLocTo.getID(), geoLocTo);
+                    synchronized (map) {
+                        map.addVertex(geoLocTo);
+                    }
+                }
+
+                synchronized (map) {
+                    Edge edge = map.addEdge(
+                            geoLocs.get(geoLocFrom.getID()),
+                            geoLocs.get(geoLocTo.getID()));
+                    map.setEdgeWeight(edge, road.getDistance());
+                    edges.add(edge);
+                }
+
             });
 
-            Object[] distances = _roads.keySet().stream().sorted().toArray();
-            for (int i = 0; i < 3; i++) {
-                String sql = "INSERT into Road values(%d, %d, %.3f, '%02d:%02d:%02d');";
-                Road road = _roads.get((Double) distances[i]);
-
-                long time_in_sec = Math.round(road.getDistance() / (80.0 / 2.0) * 3600.0);
-                long hours = time_in_sec / 3600;
-                long minutes = (time_in_sec - hours * 3600) / 60;
-                long secunds = (time_in_sec - hours * 3600 - minutes * 60);
-
-                String F = String.format(sql, road.getFrom(), road.getTo(), road.getDistance(), hours, minutes, secunds);
-                String T = String.format(sql, road.getTo(), road.getFrom(), road.getDistance(), hours, minutes, secunds);
-
-                dbConnection.sendInsertSQL(F);
-                dbConnection.sendInsertSQL(T);
-
-                try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("db_scripts/create_arlas_db.sql", true)))) {
-                    out.println(F);
-                    out.println(T);
-                } catch (IOException e) {
-                    System.out.println(e);
+            ArrayList<Set<GeoLoc>> cycleOfVertex = new ArrayList<Set<GeoLoc>>();
+            geoLocs.forEach((aLong, geoLoc) -> {
+                boolean geoLocIsInCycle = cycleOfVertex.stream().anyMatch(geoLocs1 -> geoLocs1.contains(geoLoc));
+                ArrayList<Set<GeoLoc>> t = cycleOfVertex;
+                if (!geoLocIsInCycle) {
+                    cycleOfVertex.add(new CycleDetector<GeoLoc, Edge>(map).findCyclesContainingVertex(geoLoc));
                 }
+            });
+
+            System.out.println(String.format("There is %d cycle(s)", cycleOfVertex.size()));
+
+            if (cycleOfVertex.size() > 1) {
+                HashMap<Double, Road> _roads = new HashMap<>();
+
+                cycleOfVertex.stream().forEach(cycleA -> {
+                    cycleOfVertex.stream().forEach((cycleB) -> {
+                        if (cycleA != cycleB) {
+                            cycleA.stream().forEach(geoLocA -> {
+                                cycleB.stream().forEach(geoLocB -> {
+                                    Double d = distFrom(geoLocA.getLocation(), geoLocB.getLocation());
+                                    if (d >= 0.1) {
+                                        _roads.put(d, new Road(geoLocA.getID(), geoLocB.getID(), d, null));
+                                    }
+                                });
+                            });
+                        }
+                    });
+                });
+
+                Object[] distances = _roads.keySet().stream().sorted().toArray();
+                for (int i = 0; i < 3; i++) {
+                    String sql = "INSERT into Road values(%d, %d, %.3f, '%02d:%02d:%02d');";
+                    Road road = _roads.get((Double) distances[i]);
+
+                    long time_in_sec = Math.round(road.getDistance() / (80.0 / 2.0) * 3600.0);
+                    long hours = time_in_sec / 3600;
+                    long minutes = (time_in_sec - hours * 3600) / 60;
+                    long secunds = (time_in_sec - hours * 3600 - minutes * 60);
+
+                    String F = String.format(sql, road.getFrom(), road.getTo(), road.getDistance(), hours, minutes, secunds);
+                    String T = String.format(sql, road.getTo(), road.getFrom(), road.getDistance(), hours, minutes, secunds);
+
+                    dbConnection.sendInsertSQL(F);
+                    dbConnection.sendInsertSQL(T);
+
+                    try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("db_scripts/create_arlas_db.sql", true)))) {
+                        out.println(F);
+                        out.println(T);
+                    } catch (IOException e) {
+                        System.out.println(e);
+                    }
+                }
+            } else {
+                break;
             }
         }
 
