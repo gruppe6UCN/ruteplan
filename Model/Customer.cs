0be578fdd3b7829f5346b3ad81b95a0a5be96906
JRiis <1034481@ucn.dk>
2015-09-24 12:48:56 +0200

    Test For Customer

--- Model/Customer.cs
+++ Model/Customer.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DateTime; 
 
 namespace Model
 {
     class Customer
     {
+        public long ID { get; private set; }
+        public String StreetName { get; private set; }
+        public String StreetNo { get; private set; }
+        public int Zipcode { get; private set; }
+        public String City { get; private set; }
+        public DateTime TimeOfDelivery { get; private set; }
+
+        public Customer(long ID, String StreetName, String StreetNo, int Zipcode, String City, DateTime TimeOfDelivery)
+        {
+            this.ID = ID;
+            this.StreetName = StreetName;
+            this.StreetNo = StreetNo;
+            this.Zipcode = Zipcode;
+            this.City = City;
+            this.TimeOfDelivery = TimeOfDelivery;
+        }
     }
 }
--- TestModel/TestCustomer.cs
+++ TestModel/TestCustomer.cs
@@ -1,12 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NUnit.Framework;
+using Model;
 
 namespace TestModel
 {
-    class TestCustomer
+     [TestFixture()]
+
+    public class TestCustomer
     {
+         Customer c;
+
+         [SetUp()]
+         public void SetUp()
+         {
+             c = new Customer(10, "Rejegade", "5", 9080,"Aalborg", 12);
+         }
+
+         [Test()]
+         //Test for Customer ID
+         public void TestID()
+         {
+             Assert.AreEqual(c.ID, 10);
+         }
+         //Test for Customer StreetName
+         public void TestStreetName()
+         {
+             Assert.AreEqual(c.StreetName, "Rejegade");
+         }
+         //Test for Customer StreetNumber
+         public void TestStreetNo()
+         {
+             Assert.AreEqual(c.StreetNo, "5");
+         }
+         //Test for Customer Zipcode
+         public void TestZipcode()
+         {
+             Assert.AreEqual(c.Zipcode, 9080);
+         }
+         //Test for Customer City
+         public void TestCity()
+         {
+             Assert.AreEqual(c.City, "Aalborg");
+         }
+         //Test for Time of Delivery
+         public void TestTimeOfDelivery()
+         {
+             Assert.AreEqual(c.TimeOfDelivery, 12);
+         }
+        
+
     }
 }
