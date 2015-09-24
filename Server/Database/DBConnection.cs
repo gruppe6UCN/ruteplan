bbab29c9ff1733fdee547f82965be61eab7fb446
Dennis <1038048@ucn.dk>
2015-09-24 13:50:15 +0200

    Working on unittest for database

---
+++ Server/Database/DBConnection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.IO;
+
+namespace Server
+{
+    public class DBConnection
+    {
+        private static DBConnection instance;
+        private MySqlConnection connection;
+
+        public String Host { get; set; }
+        public String DB { get; set; }
+        public String User { get; set; }
+        public String Pass { get; set; }
+        private const String connectionString = "SERVER={0};DATABASE={1};UID={2};PASSWORD={3};";
+
+        private DBConnection()
+        {
+            if (Host == null)
+                throw new NullReferenceException("You need to set the Host");
+            connection = new MySqlConnection(String.Format(connectionString, Host, DB, User, Pass));
+        }
+
+        public void Connect()
+        {
+            connection.Open();
+        }
+
+        public void Disconnect()
+        {
+            connection.Clone();
+        }
+    }
+}
+
