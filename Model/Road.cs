5ff329491ba432a0b8bfef46f3cc8102b130db75
JRiis <1034481@ucn.dk>
2015-09-25 09:50:44 +0200

    Test og Model for Road (Mangler Lidt)

--- Model/Road.cs
+++ Model/Road.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    class Road
+    public class Road
     {
+        public long from { get; private set; }
+        public long to { get; private set; }
+        public double distance { get; private set; }
+        public DateTime time { get; private set; }
+
+        public Road(long from_ID, long to_ID, double distance, DateTime time)
+        {
+            this.from = from_ID;
+            this.to = to_ID;
+            this.distance = distance;
+            this.time = time;
+        }
     }
+
 }
--- TestModel/TestRoad.cs
+++ TestModel/TestRoad.cs
@@ -1,12 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NUnit.Framework;
+using Model;
 
 namespace TestModel
 {
-    class TestRoad
+    [TestFixture()]
+    public class TestRoad
     {
+        Road ro;
+
+        [SetUp()]
+        public void Setup()
+        {
+            ro = new Road();
+        }
+        //Test 
+        [Test()]
+        public void TestFrom()
+        {
+            Assert.AreEqual(ro.from, );
+        }
+        //Test
+        [Test()]
+        public void TestTo()
+        {
+            Assert.AreEqual(ro.to, );
+        }
+        //Test
+        [Test()]
+        public void TestDistance()
+        {
+            Assert.AreEqual(ro.distance, );
+        }
+        //Test
+        [Test()]
+        public void TestTime()
+        {
+            Assert.AreEqual(ro.time, );
+        }
+               
+        
     }
 }
