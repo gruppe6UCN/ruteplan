c73a370c913f234b2345643c1b3e4d2a3726ca33
JRiis <1034481@ucn.dk>
2015-09-25 10:15:52 +0200

    Test og Model for Deliverystop (mangler lidt)

--- Model/DeliveryStop.cs
+++ Model/DeliveryStop.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    class DeliveryStop
+    public class DeliveryStop
     {
+        public long id { get; private set; }
+        public DefaultDeliveryStop defaultStop { get; private set; }
+        public List<TransportUnit> transportUnits { get; private set; }
+
+        public DeliveryStop(DefaultDeliveryStop defaultStop)
+        {
+            this.defaultStop = defaultStop;
+            //Automatize some variables such as id later m8.
+        }
+
+        /**
+         *
+         * @return the sum of transport units for this stop
+         */
+        public double GetSizeOfTransportUnits() {
+        //Creates variable.
+        double Load = 0.0;
+
+        //Enters a loop for each transportUnit
+        for (TransportUnit TransportUnit:TransportUnits) {
+
+            //Increments load with the transportUnits size.
+            Load += TransportUnit.GetUnitType();
+        }
+
+        //Returns the load.
+        return Load;
     }
+  }
+
 }
