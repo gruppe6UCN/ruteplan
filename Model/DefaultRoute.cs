dd933e65a26a009ea1cd8df43c8055dda7281e31
JRiis <1034481@ucn.dk>
2015-09-25 10:59:49 +0200

    Default Route
    DefaultRoute Modellayer

--- Model/DefaultRoute.cs
+++ Model/DefaultRoute.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    class DefaultRoute
+    public class DefaultRoute
     {
+        public long id { get; private set; }
+        public TrailerType trailerType { get; private set; }
+        public Boolean extraRoute { get; private set; }
+
+        public DefaultRoute(long id, TrailerType trailerType, Boolean extraRoute)
+        {
+            this.id = id;
+            this.trailerType = trailerType;
+            this.extraRoute = extraRoute;
+        }
+
+        public DefaultRoute(TrailerType trailerType, Boolean extraRoute)
+        {
+            this.trailerType = trailerType;
+            this.extraRoute = extraRoute;
+        }
+
+      
+        /**
+         * @return the extraRoute
+         */
+        public Boolean isExtraRoute()
+        {
+            return extraRoute;
+        }
     }
 }
