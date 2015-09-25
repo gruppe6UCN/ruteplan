4e476944586a26802b84cc60ce341fde74bc5244
Dennis <1038048@ucn.dk>
2015-09-25 12:00:39 +0200

    Can now connect to DB and unittest was create

--- Server/Database/DBConnection.cs
+++ Server/Database/DBConnection.cs
@@ -15,22 +15,39 @@ namespace Server
         public String Pass { get; set; }
         private const String connectionString = "SERVER={0};DATABASE={1};UID={2};PASSWORD={3};";
 
+        public bool IsConnected { get {return connection.State == System.Data.ConnectionState.Open; } }
+
+        public static DBConnection Instance { 
+            get { 
+                if (instance == null)
+                    instance = new DBConnection(); return instance;
+            }
+        }
+
         private DBConnection()
         {
-            if (Host == null)
-                throw new NullReferenceException("You need to set the Host");
-            connection = new MySqlConnection(String.Format(connectionString, Host, DB, User, Pass));
+            connection = new MySqlConnection();
         }
 
         public void Connect()
         {
+            if (Host == null)
+                throw new NullReferenceException("You need to initialize the accessor 'Host'");
+            else if (DB == null)
+                throw new NullReferenceException("You need to initialize the accessor 'DB'");
+            else if (User == null)
+                throw new NullReferenceException("You need to initialize the accessor 'User'");
+            else if (Pass == null)
+                throw new NullReferenceException("You need to initialize the accessor 'Pass'");
+
+
+            connection.ConnectionString = String.Format(connectionString, Host, DB, User, Pass);
             connection.Open();
         }
 
         public void Disconnect()
         {
-            connection.Clone();
+            connection.Close();
         }
     }
 }
-
