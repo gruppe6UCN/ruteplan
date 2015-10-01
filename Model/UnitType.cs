a8e544afde3a7604dcc298ee6d2bf08db5813180
JRiis <1034481@ucn.dk>
2015-10-01 11:55:38 +0200

    UnitType Model

--- Model/UnitType.cs
+++ Model/UnitType.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    class UnitType
+    public enum UnitType
     {
+        EU_PAL = 1,
+        HALV_PAL = 0.5,
+        KVART_PAL = 0.25,
+        RULLE_PALLE = 0.70,
+        VARE_CONTAINER = 0.44,
+        SMOER_CONTAINER = 0.40,
+        MAELKE_CONTAINER = 0.40
     }
 }
