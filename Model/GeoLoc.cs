483742e6bd05d6fa6fcb67bd1de8b950db10e530
Filmting <green@tjekmig.dk>
2015-10-07 11:01:42 +0200

    Added GeoLoc and Point

---
+++ Model/GeoLoc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Model
+{
+    public class GeoLoc : ISerializable
+    {
+        public long ID { set; private get; }
+        public Point location { set; private get; }
+
+        public GeoLoc(long id, double latitude, double longitude)
+        {
+            this.ID = id;
+            this.location = new Point(latitude, longitude);
+        }
+
+    }
+}
