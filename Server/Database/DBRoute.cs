b471fffddf8a163d5cc5494adc1c252bb240795a
Filmting <green@tjekmig.dk>
2015-10-21 13:11:08 +0200

    Added 2 new methods to class DBRoute, that allows for retriving of routes from database.

--- Server/Database/DBRoute.cs
+++ Server/Database/DBRoute.cs
@@ -10,6 +10,23 @@ namespace Server.Database
 {
     public class DBRoute
     {
+        class TmpRoute
+        {
+            public long ID { get; set; }
+            public long DefaultRouteID { get; private set; }
+            public List<DeliveryStop> Stops { get; private set; }
+            public DateTime TimeForDeparture { get; private set; }
+            public DateTime DateForDeparture { get; private set; }
+
+            public TmpRoute(long DefaultRouteID, DateTime Date, DateTime time)
+            {
+                this.DefaultRouteID = DefaultRouteID;
+                this.DateForDeparture = Date;
+                this.TimeForDeparture = time;
+                this.Stops = new List<DeliveryStop>();
+            }
+        }
+        
         public DBConnection DbConnection { get; private set; }
         private static DBRoute instance; 
 
@@ -32,8 +49,52 @@ namespace Server.Database
 
                 return instance;
             }
-
         }
+
+        //Returns a list of all routes.
+        //TODO - Optimize with lambda...
+        public List<Route> GetRoutes(List<DefaultRoute> defaultRoutes)
+        {
+            List<TmpRoute> tmpList;
+            string sql = string.Format("select * from Route;");
+            tmpList = DbConnection.SendSQL<TmpRoute>(sql, ConvertToRoute);
+
+            List<Route> list = new List<Route>();
+
+            defaultRoutes.ForEach(s =>
+            {
+                foreach (TmpRoute tmpRoute in tmpList)
+                {
+                    if (tmpRoute.DefaultRouteID == s.ID)
+                    {
+                        Route route = new Route(s, tmpRoute.DateForDeparture, tmpRoute.TimeForDeparture);
+                        route.ID = tmpRoute.ID;
+                        list.Add(route);
+                    }
+                }
+            });
+
+            return list;
+        }
+
+
+        // Converts database string to a model class.
+        private List<TmpRoute> ConvertToRoute(IDataReader dataSet)
+        {
+            List<TmpRoute> tableList = new List<TmpRoute>();
+            while (dataSet.Read())
+            {
+                tableList.Add(new TmpRoute(
+
+                    dataSet.GetInt64(0),
+                    dataSet.GetDateTime(1),
+                    dataSet.GetDateTime(2)
+                    ));
+            }
+
+            return tableList;
+        }  
+
 
         /// Stores all routes in the database.
         public long storeRoute(Route route)
