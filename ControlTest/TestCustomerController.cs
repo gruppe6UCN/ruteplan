5f7856df44b0248523667d6fa5fab2480226b694
JRiis <1034481@ucn.dk>
2015-10-22 09:54:24 +0200

    Control

--- ControlTest/TestCustomerController.cs
+++ ControlTest/TestCustomerController.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using Control;
+using Server; 
 
 namespace ControlTest
 {
+    [TestFixture()]
     class TestCustomerController
     {
+        CustomerController cc; 
+
+        [SetUp()]
+        public void SetUp()
+        {
+            this.cc = new CustomerController(); 
+        }
+
     }
 }
