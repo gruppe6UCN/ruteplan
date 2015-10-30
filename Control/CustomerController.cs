01a9812500788f4ee596fa5ecd2f04022a6cffcb
JRiis <1034481@ucn.dk>
2015-10-30 10:25:51 +0100

    CustomerController

--- Control/CustomerController.cs
+++ Control/CustomerController.cs
@@ -9,5 +9,40 @@ namespace Control
 {
     public class CustomerController
     {
+        public DBCustomer DbCustomer { get; private set; }
+        private static CustomerController instance;
+
+        /**
+         * Private constructor for singleton.
+         */
+        private CustomerController()
+        {
+            DbCustomer = DBCustomer.Instance;
+        }
+
+        /**
+         * Singleton method for class.
+         * @return instance of class.
+         */
+        public static CustomerController Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new CustomerController();
+                }
+                return instance;
+            }
+        }
+
+        /// <summary>
+        /// Adds customers to the given default delivery stop.
+        /// </summary>
+        /// <param name="defaultStop"></param>
+        public void AddCustomers(DefaultDeliveryStop defaultStop)
+        {
+            defaultStop.Customers = (DbCustomer.GetCustomers(defaultStop.ID));
+        }
     }
 }
