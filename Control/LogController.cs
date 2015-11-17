a5fa6452a9ce3f2ee5b56914e259e995a918bdbc
Dennis Værum <dennis.vaerum@gmail.com>
2015-11-17 12:43:30 +0100

    Merge LogController from the branch GMap

--- Control/LogController.cs
+++ Control/LogController.cs
@@ -1,12 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Control
 {
     public class LogController
     {
+        private static LogController instance;
+
+        private int maxLogLength = 1000;
+        private Collection<String> logReceiver = null;
+
+        /// <summary>
+        /// Singleton method. Returns the instance of the class.
+        /// </summary>
+        /// <returns>Instance of class.</returns>
+        public static LogController Instance { 
+                get { 
+                    if (instance == null)
+                        instance = new LogController();
+                    return instance;
+                }
+            }
+
+        /// <summary>
+        /// Private singleton constructor.
+        /// </summary>
+        private LogController() {
+        }
+
+        public void setLogReceiver(Collection<String> logReceiver)
+        {
+            this.logReceiver = logReceiver;
+        }
+
+        public void setMaxLogLength(int maxLogLength) {
+            this.maxLogLength = maxLogLength;
+        }
+
+
+        public void StatusLog(String log) {
+            if (logReceiver != null) {
+                    //logReceiver.Insert(0, log);
+                    maintainLog();
+            } else {
+                Console.WriteLine(log);
+            }
+        }
+
+        private void maintainLog() {
+            if (logReceiver.Count > maxLogLength) {
+                logReceiver.Remove(logReceiver.Last());
+            }
+        }
     }
 }
