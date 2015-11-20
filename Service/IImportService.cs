10321fbd6ace3432c74eb8e780fcf230bad365f7
Filmting <green@tjekmig.dk>
2015-11-20 12:31:07 +0100

    Started working on the service layer.

---
+++ Service/IImportService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace Service
+{
+    [ServiceContract]
+    public interface IImportService
+    {
+        [OperationContract]
+        string Import(); //TODO: Everything... Also not use a string.
+    }
+}
---
+++ Service/ImportService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    class ImportService : IImportService
+    {
+        private static IImportService IMS = new ImportService();
+
+        //TODO: Everything... Also not use a string.
+        public string Import()
+        {
+            return "Nej, du blev snydt, thihi fniz. #Putin!"
+        }
+
+    }
+}
