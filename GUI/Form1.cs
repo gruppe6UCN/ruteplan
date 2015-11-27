e0ee06b5cf1f596ac0298bc5c583e540ca709671
Filmting <green@tjekmig.dk>
2015-11-27 13:42:15 +0100

    Changed from Controller on GUI to WCF Serviceszz

--- GUI/Form1.cs
+++ GUI/Form1.cs
@@ -8,17 +8,15 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Control;
-using Server.Database;
 using Model;
 using System.IO;
+using WCFService;
+using Server;
 
 namespace GUI
 {
     public partial class Form1 : Form
     {
-        string user;
-        string pass;
         public delegate void ImportFest();
         public delegate void OptimizeThread();
 
@@ -50,27 +48,19 @@ namespace GUI
         }
         public void ImportStart()
         {
-            //DB Connection
-            Server.DBConnection instance = Server.DBConnection.Instance;
-            try
-            {
-                user = File.ReadAllText("Config/user.txt");
-                pass = File.ReadAllText("Config/pass.txt");
-            }
-            catch { }
 
-            instance.Host = "localhost";
-            instance.DB = "TestArla";
-            instance.User = user;
-            instance.Pass = pass;
-            instance.Connect();
-
+            Server.WCFServer.Initialize();
+            Server.WCFServer.StartServer();
             
-            
-            ImportController.Instance.ImportRoutes();
+            ServiceImport.IServiceImport importClient = new ServiceImport.ServiceImportClient();
+            ServiceRoute.IServiceRoute routeClient = new ServiceRoute.ServiceRouteClient();
+
+            importClient.Import();
+
+            Route[] routes = routeClient.GetRoutes();
 
 
-            foreach (Route route in RouteController.Instance.Routes)
+            foreach (Route route in routes)
             {
                 //Tilføjer Rows 
                 this.dataGridView1.Rows.Add(
@@ -93,42 +83,42 @@ namespace GUI
             tabControl1.SelectedTab = tabPage2;
 
 
-            Thread t = new Thread(new ThreadStart(OptimizeThreadStart));
-            t.Start();
+            //Thread t = new Thread(new ThreadStart(OptimizeThreadStart));
+            //t.Start();
 
         }
 
-        //Delegate til at sørge for at det hele bliver i en Thread
-        public void OptimizeThreadStart()
-        {
-            OptimizeThread optimize = new OptimizeThread(OptimizeStart);
-            this.BeginInvoke(optimize);
+        ////Delegate til at sørge for at det hele bliver i en Thread
+        //public void OptimizeThreadStart()
+        //{
+        //    OptimizeThread optimize = new OptimizeThread(OptimizeStart);
+        //    this.BeginInvoke(optimize);
 
-        }
+        //}
 
-        public void OptimizeStart()
-        {
+        //public void OptimizeStart()
+        //{
         
-            OptimizeController.Instance.Optimize();
+        //    OptimizeController.Instance.Optimize();
 
 
-            foreach (Route route in RouteController.Instance.Routes)
-            {
-                //Tilføjer Rows 
-                this.dataGridView2.Rows.Add(
-                    route.DefaultRoute.ID.ToString(),
-                    route.Stops.Count.ToString(),
-                    string.Format("{0}/{1}",
-                        route.GetLoadForTrailer(),
-                        route.DefaultRoute.TrailerType),
-                    route.DateForDeparture.TimeOfDay.Hours.ToString(),
-                    route.DefaultRoute.ExtraRoute.ToString()
-                    );
+        //    foreach (Route route in RouteController.Instance.Routes)
+        //    {
+        //        //Tilføjer Rows 
+        //        this.dataGridView2.Rows.Add(
+        //            route.DefaultRoute.ID.ToString(),
+        //            route.Stops.Count.ToString(),
+        //            string.Format("{0}/{1}",
+        //                route.GetLoadForTrailer(),
+        //                route.DefaultRoute.TrailerType),
+        //            route.DateForDeparture.TimeOfDay.Hours.ToString(),
+        //            route.DefaultRoute.ExtraRoute.ToString()
+        //            );
                 
-            }
+        //    }
 
 
-        }
+        //}
 
         public void button3_Click(object sender, EventArgs e)
         {
