295597f7bfe15bca441b29409c3d4610055923c6
Dennis Værum <dennis.vaerum@gmail.com>
2015-12-02 11:57:40 +0100

    Working on getting ServiceMap To Work

---
+++ WCFService/IServiceMap.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using GMap.NET;
+using Model;
+
+namespace WCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IServiceMap" in both code and config file together.
+    [ServiceContract]
+    public interface IServiceMap
+    {
+        [OperationContract]
+        //List<MapRoute> GetRoadMap(Route route);
+        MapRoute GetRoadMap(Route route);
+    }
+}
---
+++ WCFService/ServiceMap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Control;
+using GMap.NET;
+using Model;
+
+namespace WCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ServiceMap" in both code and config file together.
+    public class ServiceMap : IServiceMap
+    {
+        //public List<MapRoute> GetRoadMap(Route route)
+        public MapRoute GetRoadMap(Route route)
+        {
+            return MapController.Instance.GetCalcRoad(route)[0];
+        }
+    }
+}
